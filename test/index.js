f8d114a5a4f44ff53d07aaa35eeaf24cad3c7c0f
Shaady Dawood <shady.youssef@gmail.com>
2015-05-13 01:58:46 +1000

    As good as jsonpath handler can handle new helper 'subscript-child-keyword_literal' var jp = require('jsonpath') jp.handlers._fns['subscript-child-keyword_literal'] =   jp.handlers._fns['member-child-identifier']; var fp = require('./lib/jsonpathplus') fp.query({ json: {null: 'null', false: 'false'}}, "json[null, false]") --------- > [ 'null', 'false' ]

--- test/index.js
+++ test/index.js
@@ -91,6 +91,7 @@ describe("#parse", function() {
 
     it('Should parse null, true, false, and undefined keys and should not coerce it into a string', function() {
         var path = parse("genreLists[null][true][false][undefined]");
+        path.original = "genreLists['null']['true']['false']['undefined']"
         pathsEqual(path, ["genreLists", null, true, false, undefined]);
     });     
 
